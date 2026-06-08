df25ce4fa7ef0f22e7f1dbead57f1fc81fbbe298
Eric-cha1 <eric@teamwise.dooray.com>
2026-06-08 16:54:14 +0900

    feat: add main.dart for Just Do It Widget application

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+import 'viewmodels/todo_viewmodel.dart';
+import 'viewmodels/window_viewmodel.dart';
+import 'views/screens/dashboard_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  const windowOptions = WindowOptions(
+    size: Size(700, 500),
+    minimumSize: Size(300, 400),
+    center: true,
+    backgroundColor: Colors.transparent,
+    titleBarStyle: TitleBarStyle.hidden,
+    skipTaskbar: false,
+  );
+
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.setAsFrameless();
+    await windowManager.setBackgroundColor(Colors.transparent);
+    await windowManager.setAlwaysOnBottom(true);
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(const JustDoItApp());
+}
+
+class JustDoItApp extends StatelessWidget {
+  const JustDoItApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TodoViewModel()),
+        ChangeNotifierProvider(create: (_) => WindowViewModel()),
+      ],
+      child: MaterialApp(
+        title: 'Just Do It Widget',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.indigo,
+            brightness: Brightness.light,
+          ),
+        ),
+        themeMode: ThemeMode.light,
+        home: const DashboardScreen(),
+      ),
+    );
+  }
+}
